b0912c261ef3d1e4a7ab2125128ad6fcac6e07cd
Itay Weintraub <7468526-itay100@users.noreply.gitlab.com>
2022-03-07 13:27:45 +0200

    I think its working :)

--- app/src/main/java/com/example/betaapp/FormStudConfirmActivity.java
+++ app/src/main/java/com/example/betaapp/FormStudConfirmActivity.java
@@ -3,18 +3,25 @@ package com.example.betaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FormStudConfirmActivity extends AppCompatActivity {
+    EditText numberBrothers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_stud_confirm);
 
+        numberBrothers = (EditText) findViewById(R.id.numberBrothers);
+
         Toast.makeText(FormStudConfirmActivity.this, Helper.studentFinishYear, Toast.LENGTH_SHORT).show();
+
+        // all the numeric fields accepts just numbers
+        numberBrothers.setTransformationMethod(null);
     }
 }
